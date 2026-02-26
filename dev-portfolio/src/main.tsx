f1da9113aaa44d3402e4c9ac08509ede8411ab99
Shashwat Singh <shashwatsinghdit@gmail.com>
2026-02-26 18:35:06 +0530

    changes for build failure

--- dev-portfolio/src/main.tsx
+++ dev-portfolio/src/main.tsx
@@ -6,7 +6,13 @@ import { ThemeProvider } from "./context/Themeconstext"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")
+
+if (!rootElement) {
+  throw new Error("Root element not found")
+}
+
+ReactDOM.createRoot(rootElement).render((
   <React.StrictMode>
     
       <ThemeProvider>
@@ -16,4 +22,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       </ThemeProvider>
     
   </React.StrictMode>
-)
+))
--- dev-portfolio/vite.config.ts
+++ dev-portfolio/vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
-  base: "/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
